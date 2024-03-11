aa3d56e3fac73eba6161ad3a32a02cd7a134b1eb
mohamamd <mohammad.hosein3135@gmail.com>
2024-03-11 22:37:41 +0330

    create login page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,13 @@
 const hamburgerBtn = document.querySelector('#hamburger-btn');
 const mobileMenuEl = document.querySelector('#mobile-menu');
-
+const navLinks = document.querySelectorAll('.nav-link')
 hamburgerBtn.addEventListener('click', () => {
     let hamburgerIcon = 'ri-menu-line text-2xl font-bold';
     mobileMenuEl.classList.toggle('show');
+
+    navLinks.forEach(item => item.addEventListener('click', () => {
+        mobileMenuEl.classList.contains('show') ? mobileMenuEl.classList.remove('show')
+            : console.log('faslse');
+    }))
 });
+
